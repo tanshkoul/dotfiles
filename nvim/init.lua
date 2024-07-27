dd4da08593c7d2867c4383e817d5c9c6bc856dba
Tansh Koul <147000690+tanshkoul@users.noreply.github.com>
2024-07-27 15:35:08 +0530

    Add initial neovim config

---
+++ nvim/init.lua
@@ -0,0 +1,14 @@
+-- Import Lua modules
+require('core/lazy')
+require('core/autocmds')
+require('core/keymaps')
+require('core/colors')
+require('core/statusline')
+require('core/options')
+require('lsp/lspconfig')
+require('plugins/nvim-tree')
+require('plugins/indent-blankline')
+require('plugins/nvim-cmp')
+require('plugins/nvim-treesitter')
+require('plugins/alpha-nvim')
+
