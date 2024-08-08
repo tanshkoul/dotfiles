b6193662a07e733d7a219464402ddb61266e56af
Tansh Koul <koultansh@gmail.com>
2024-08-08 14:31:30 +0530

    Change color

--- nvim/init.lua
+++ nvim/init.lua
@@ -13,12 +13,12 @@ require('plugins/nvim-treesitter')
 require('plugins/alpha-nvim')
 
 
--- Set background color
-vim.cmd('highlight Normal guibg=#000000')
-vim.cmd('highlight NormalNC guibg=#000000')
-vim.cmd('highlight SignColumn guibg=#000000')
-vim.cmd('highlight EndOfBuffer guibg=#000000')
-vim.cmd('highlight LineNr guibg=#000000')
-vim.cmd('highlight NvimTreeNormal guibg=#000000')
-vim.cmd('highlight NvimTreeEndOfBuffer guibg=#000000')  -- Ensure NvimTree end of buffer matches
-vim.opt.fillchars:append { eob = " " }
+-- -- Set background color
+-- vim.cmd('highlight Normal guibg=#000000')
+-- vim.cmd('highlight NormalNC guibg=#000000')
+-- vim.cmd('highlight SignColumn guibg=#000000')
+-- vim.cmd('highlight EndOfBuffer guibg=#000000')
+-- vim.cmd('highlight LineNr guibg=#000000')
+-- vim.cmd('highlight NvimTreeNormal guibg=#000000')
+-- vim.cmd('highlight NvimTreeEndOfBuffer guibg=#000000')  -- Ensure NvimTree end of buffer matches
+-- vim.opt.fillchars:append { eob = " " }
